aad8bce9fe56fa3d37163334a9f2e5520a222b37
deivis <deivis.rodriguez2@o365.unab.edu.co>
2022-11-04 08:23:58 -0500

    clase 7

--- src/main/java/edu/mintic/tripulantes/controllers/TripulanteController.java
+++ src/main/java/edu/mintic/tripulantes/controllers/TripulanteController.java
@@ -2,9 +2,15 @@ package edu.mintic.tripulantes.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.mintic.tripulantes.entity.Tripulante;
@@ -20,13 +26,35 @@ public class TripulanteController {
     }
 
     @GetMapping("/tripulantes")
-    public List<Tripulante> listaTripulantes(){
+    public List<Tripulante> listaTripulantes() {
         return repository.findAll();
     }
 
+    @GetMapping("/tripulantes/{idTripulante}")
+    public Tripulante getTripulante(@PathVariable int idTripulante) {
+        return repository.findById(idTripulante).orElse(null);
+    }
+
     @PostMapping("/tripulantes")
-    public Tripulante crearTripulante(@RequestBody Tripulante tripulante){
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Tripulante crearTripulante(@RequestBody Tripulante tripulante) {
+        
         return repository.save(tripulante);
+    }
+
+    @PutMapping("/tripulantes/{idTripulante}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Tripulante actualizarTripulante(@RequestBody Tripulante tripulante, @PathVariable int idTripulante) {
+        Tripulante tViejo = repository.findById(idTripulante).orElseThrow();
+        tViejo.setNombre(tripulante.getNombre());
+        tViejo.setCursos(tripulante.getCursos());
+        return repository.save(tViejo);
+    }
+
+    @DeleteMapping("/tripulantes")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void eliminarTripulante(@RequestParam int idTripulante) {
+        repository.deleteById(idTripulante);
     }
 
 }
--- src/main/java/edu/mintic/tripulantes/entity/Tripulante.java
+++ src/main/java/edu/mintic/tripulantes/entity/Tripulante.java
@@ -1,31 +1,53 @@
 package edu.mintic.tripulantes.entity;
 
+import java.util.List;
+
+//import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
 
 @Entity
+//@Table(name = "tbl_nombre_tabla")
 public class Tripulante {
     private @Id @GeneratedValue int idTripulante;
+    //@Column(name = "columna_nombre_tripulante")
     private String nombre;
-    private String curso;
+    @OneToMany(mappedBy = "tripulante")
+    private List<Curso> cursos;
 
     public Tripulante() {
     }
 
-    public Tripulante(String nombre, String curso) {
+    public Tripulante(String nombre, List<Curso> curso) {
         this.nombre = nombre;
-        this.curso = curso;
+        this.cursos = curso;
     }
 
-    public String getNombre(){
+    public String getNombre() {
         return this.nombre;
     }
-    public String getCurso(){
-        return this.curso;
+
+    public List<Curso> getCursos() {
+        return this.cursos;
     }
-    public int getIdTripulante(){
+
+    public int getIdTripulante() {
         return this.idTripulante;
+    }
+
+    public void setIdTripulante(int idTripulante) {
+        this.idTripulante = idTripulante;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
     }
 
 }
